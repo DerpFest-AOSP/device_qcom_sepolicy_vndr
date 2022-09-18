DerpFest-AOSP/device_qcom_sepolicy

sepolicy_vndr: Include DerpFest-specific QCOM sepolicy
Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00

SEPolicy.mk
@@ -72,3 +72,5 @@ ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test/sysmonapp
     endif
 endif
+
+-include device/aosip/sepolicy/qcom/sepolicy.mk

DerpFest-AOSP/device_qcom_sepolicy_vndr

SEPolicy.mk
@@ -75,3 +75,5 @@ ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
       BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test/mst_test_app
     endif
 endif
+
+-include device/derp/sepolicy/qcom/sepolicy.mk